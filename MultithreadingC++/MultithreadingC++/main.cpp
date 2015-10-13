1634d29b6137ae8a078f66f4a0febd884fd99110
Calvin Truong <uoi100x@gmail.com>
2015-10-12 18:49:05 -0700

    C++ Implementation

---
+++ MultithreadingC++/MultithreadingC++/main.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+}
